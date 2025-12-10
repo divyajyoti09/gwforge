8cb278a3ceab626625ee0e5650123ec991b734a1
Divyajyoti <50514405+divyajyoti09@users.noreply.github.com>
2025-12-10 16:43:58 -0500

    Improve error message formatting for redshift model

--- GWForge/population/redshift.py
+++ GWForge/population/redshift.py
@@ -115,7 +115,7 @@ class Redshift:
             from gwpopulation.models.redshift import PowerLawRedshift
             psi_of_z = PowerLawRedshift(z_max=self.maximum_redshift).psi_of_z(redshift=z, **self.parameters)
         else:
-            raise ValueError('Redshift model {} is not implemented in GWPopulation')
+            raise ValueError(f'Redshift model {self.redshift_model} is not implemented in GWPopulation')
     
         return psi_of_z * population_models.p_tau(tau=time_delay, 
                                           td_model=self.time_delay_model) * diff_lookback_time(z)
